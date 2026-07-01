6f3375fd70bc3b90cdb90a1ea3562d4a38320a9d
David C <dcai1.3828@gmail.com>
2026-07-01 13:48:09 -0400

    Update Metadata Main Structure changes Add new background frames in attempt to achieve an animated background

--- ModMain.gd
+++ ModMain.gd
@@ -17,42 +17,39 @@ func _ready():
 
 # Here is where the stage building takes place.
 func build_stage():
-	
-	Builder = CustomStageBuilder.new() # For building the stage metadata
-	Loader = get_tree().get_root().get_node("CSL"); # For loading the stage itself
 
-	Builder.data.stage_name = "My First Stage"; # Setting the name of the stage
-	Builder.data.stage_icon = "res://MyFirstStage/icon.png"; # Setting the icon of the stage
+	Builder = CustomStageBuilder.new()
+	Loader = get_tree().get_root().get_node("CSL")
+
+	Builder.data.stage_name = "Animated Stage"
+	Builder.data.stage_icon = "res://MyFirstStage/icon.png"
+
+	# Animated background (355 frames)
+	Builder.make_background("AnimatedBackground", {
+		"layer": 1,
+	})
+
+	Builder.make_layer("BgLayer", Builder.get_material_id("AnimatedBackground"))
+
+	Builder.make_element("BgAnimation", Builder.get_material_id("BgLayer"), {
+		"active": true,
+		"ticks_per_frame": 2,
+		"frames": Builder.make_spriteframes_animation("res://MyFirstStage/layers/background/"),
+		"position": Vector2(0, 0),
+		"h_tile": true,
+	})
 
 	# Ground
-	Builder.make_background("GroundBackground", { # Creating a background to hold our ground layers
-		"layer": 1, # setting it's index to 1, so it renders on front of the SkyBackground.
-	});
+	Builder.make_background("GroundBackground", {
+		"layer": 1,
+	})
 
-	Builder.make_layer("GroundLayer", Builder.get_material_id("GroundBackground")); # Creating the ground layer, making it a child of GroundBackground
+	Builder.make_layer("GroundLayer", Builder.get_material_id("GroundBackground"))
 
-	Builder.make_element("Ground", Builder.get_material_id("GroundLayer"), { # Creating the ground element and changing it's data 
+	Builder.make_element("Ground", Builder.get_material_id("GroundLayer"), {
 		"frames": Builder.make_spriteframes_image("res://MyFirstStage/layers/ground.png"),
 		"position": Vector2(0, 75),
 		"h_tile": true,
-	});
+	})
 
-	## Clouds
-	Builder.make_background("SkyBackground", { # Creating a background to hold our ground layers, 
-		"layer": 0, # setting it's index to 0, so it renders on front of the SkyBackground.
-		"bg_color": Color("#B0CBFF")
-	});
-
-	Builder.make_layer("CloudsLayer", Builder.get_material_id("SkyBackground"), {# Creating the clouds layer, making it a child of MainBackground
-		"motion_scale": Vector2(0.8, 0.8),
-	});
-
-	Builder.make_element("Clouds", Builder.get_material_id("CloudsLayer"), { # Creating the cloud element and changing it's data 
-		"active": true,
-		"ticks_per_frame": 10,
-		"frames": Builder.make_spriteframes_animation("res://MyFirstStage/layers/clouds/"),
-		"position": Vector2(0, -150),
-		"h_tile": true,
-	});
-
-	Loader.add_stage(Builder.data);
+	Loader.add_stage(Builder.data)
